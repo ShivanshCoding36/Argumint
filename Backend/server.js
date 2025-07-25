beaeb142253fb5d6a554e7cb4d76693ec46f9ebd
Shivansh Mathur <101795319+ShivanshCoding36@users.noreply.github.com>
2025-07-25 19:36:42 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import judgeRouter from './routes/judge.js';
-import transcribeRouter from './routes/transcribe.js';
 import saveRouter from './routes/saveDebate.js';
 import generateTopicRouter from './routes/generateTopic.js';
 import aiResponseRouter from './routes/aiResponse.js';
-import getCredentials from './routes/assemblyai.js';
-import transcribeStreamRouter from './routes/transcribeStream.js';
 
 dotenv.config();
 
@@ -25,10 +22,7 @@ app.use(express.json());
 app.use('/api/ai-response', aiResponseRouter);
 app.use('/api/save', saveRouter);
 app.use('/api/judge', judgeRouter);
-app.use('/api/transcribe', transcribeRouter);
 app.use('/api/generate-topic', generateTopicRouter);
-app.use('/api/get-credentials', getCredentials);
-app.use('/api/stream-transcribe', transcribeStreamRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🧠 Judging API running on port ${PORT}`));
